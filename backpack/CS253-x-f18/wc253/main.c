6715f5f27db303c3fc97855919559c34d0056457
Jim Conrad <jimconrad@boisestate.edu>
2018-09-13 13:13:04 -0600

    Auto commit from Backpack

---
+++ backpack/CS253-x-f18/wc253/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+  /*
+  **Put your code here. You should create as many functions 
+  **as necessary to create a modular program. We should not 
+  **see ALL your code in the main function! 
+  **
+  **See ExampleGetopt and ExampleCharacterIO for examples of
+  **processing command-line options and character I/O from
+  **a file.
+  */
+  return 1;  //Error exit status
+}
