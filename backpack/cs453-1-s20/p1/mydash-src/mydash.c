01c892e7d716f5973d925a538da3f66266e1eefc
vannguyen <vannguyen599@u.boisestate.edu>
2020-01-27 21:36:23 -0700

    Working on project

--- backpack/cs453-1-s20/p1/mydash-src/mydash.c
+++ backpack/cs453-1-s20/p1/mydash-src/mydash.c
@@ -4,32 +4,164 @@
 #include    <sys/types.h>
 #include    <sys/wait.h>
 #include    <mydash.h>
+#include    <readline/readline.h>
+#include    <readline/history.h>
+#include    <pwd.h>
+#include    <unistd.h>
 
-int main(void)
+#define MAXLINE 4096
+void changeDir(char **token);
+const char* git_version(void);
+char** tokenizeLine(char *line);
+void freeArray(char **array);
+int main(int argc, char *argv[])
 {
-    char    buf[MAXLINE];
+  
     pid_t   pid;
     int     status;
+    char    *line;    
+    char    **cmd;    
+    int     numTokens;
+    char    *DASH_PROMPT;
 
-    printf("%% ");  /* print prompt (printf requires %% to print %) */
-    while (fgets(buf, MAXLINE, stdin) != NULL) {
-        buf[strlen(buf) - 1] = '\0';    /* replace newline with null */
-
-        if ( (pid = fork()) < 0)
-            err_sys("fork error");
-
-        else if (pid == 0) {        /* child */
-            execlp(buf, buf, (char *) 0);
-            err_ret("couldn't execute: %s", buf);
-            exit(EXIT_FAILURE);
-        }
-
-        /* parent */
-        if ( (pid = waitpid(pid, &status, 0)) < 0)
-            err_sys("waitpid error");
-        printf("%% ");
+    if(  argc==2 && strcmp(argv[1],"-v")==0)
+    {
+      printf("mydash: Version 1: Revision %s (author: VanNguyen599@onyx.boisestate.edu)\n", git_version());
+      exit(0);
+    } 
+    else if (argc==2 && strcmp(argv[1],"-v")!=0)
+    {
+      printf("Invalid Option \n");
+      exit(0);
     }
-    exit(EXIT_SUCCESS);
+    char *env = getenv("DASH_PROMPT");
+    numTokens = 0;    // the number of tokens in 1 command line
+     
+    if (env == NULL){
+      DASH_PROMPT =  "mydash>";
+    }
+    else
+    {
+      DASH_PROMPT = env;
+    }
+    using_history(); 
+    while ((line=readline(DASH_PROMPT)))
+    {
+        // Case 1: The line is empty
+        if (line[0] == '\0') 
+        {
+          continue;
+        }     
+        // Case 2: The line is not empty
+        add_history(line);
+        
+        // Case 2a: The command is "exit"
+        if(strcmp(line,"exit")==0)
+        {         
+          exit(0);
+        }
+        cmd = tokenizeLine(line);
+        // Case 2b: The command is "cd"
+        if(strcmp(cmd[0],"cd")==0)
+        {
+          changeDir(cmd);  
+          free(line);
+          continue;
+        }
+        if (strcmp(cmd[0], "-v") == 0) 
+        {
+          printf("Version: %s \n", git_version());
+          free(line);
+          break;
+	      }
+        pid = fork();
+        if(pid==-1)
+        {
+          err_ret("couldn't execute: %s", line);
+          exit(EXIT_FAILURE);
+        }
+         else if (pid == 0) 
+         { 
+          if (execvp(cmd[0],cmd)==-1)
+          {   
+            err_ret("couldn't execute: %s", *cmd);
+            exit(1);
+          }
+         freeArray(cmd);
+        }
+        freeArray(cmd);
+        if ( (pid = waitpid(pid, &status, 0)) < 0)
+        {
+            err_sys("waitpid error");
+            free(line);
+            freeArray(cmd);
+        }
+    free(line);
+    }
+    exit(0);
 }
+void changeDir(char **token)
+{
+  if (token[1])
+  {
+     int success;
+     success=chdir(token[1]);
+     if(success!=0)
+     {
+       err_ret("error:%s does not exist ",token[1]);  
+     } 
+  }
+  else
+  {
+    char *homedir = getenv("HOME");
+    if (homedir == NULL)
+    {
+        uid_t uid = getuid();
+        struct passwd *pw = getpwuid(uid);
+        char *homedir = pw->pw_dir;
+        chdir(homedir);
+    }
+    else
+    {
+        chdir(homedir);
+    }
+  }
+  freeArray(token);
+}
+char** tokenizeLine(char *line) {
+    char *nextToken;
+    int i=0;
+    char **input;
+    char *token;
+    char *temp;
+
+    temp = malloc(strlen(line) * sizeof(char) + 1);
+    strcpy(temp, line);
+    input = malloc(MAXLINE * sizeof(char*));
+    nextToken = strtok(temp, " ");
+    
+    while (nextToken != NULL) 
+    {
+      input[i] = malloc(strlen(nextToken) + 1);
+      token = input[i++];
+      strcpy(token, nextToken);
+      nextToken = strtok(NULL, " ");
+    }
+    
+    input[i] = '\0';
+    free(temp);
+    return input;
+}
+void freeArray(char **array) {
+
+  int i = 0;
+  while (array[i] != NULL) {
+    free(array[i]);
+    i++;
+  } 
+   
+  free(array);
+}
+
 
 /* vim: set ts=4: */
